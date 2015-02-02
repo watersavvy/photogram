ea24efc8ff703b058ae2f4e31fe50d4a18db54b5
Angelique S Kepshire <angelique@watersavvy.info>
2015-02-02 15:06:01 -0600

    stuck for a few on method variable errors
    I figured out what made it work which was adding the "@" in lines 6, 10
    and 16. Why is it necessary on the .show page but not on the .index
    page?

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,12 @@
 class PhotosController < ApplicationController
+  
   def index
     @list_of_photos = Photo.all
+  end
+
+# single photo view
+  def show 
+  	@photo = Photo.find_by({ :id => params["id"] })
   end
 
 
